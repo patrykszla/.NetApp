f410023a0600050508874555f6b01fba5ab6ce72
Patryk Szlachta <patryk.szla@interia.pl>
2020-10-18 22:18:25 +0200

    finish lab1 (form)

---
+++ CompanyProjectLaby/CompanyProjectLaby/Controllers/CompanyController.cs
@@ -0,0 +1,31 @@
+﻿using CompanyProjectLaby.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyProjectLaby.Controllers
+{
+    public class CompanyController : Controller
+
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CompanyModel company)
+        {
+            var viewModel = new CompanyAddedViewModel
+            {
+                NumberOfCharsInName = company.Name.Length,
+                NumberOfCharsInDescription = company.Description.Length,
+                IsHidden = !company.IsVisible
+            };
+            return View("CompanyAdded", viewModel);
+        }
+    }
+}
---
+++ CompanyProjectLaby/CompanyProjectLaby/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CompanyProjectLaby.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
